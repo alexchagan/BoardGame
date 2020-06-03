e892c77378cf0a0f17d55e3fb55b04af6c2437fc
Alex <44925899+alexchagan@users.noreply.github.com>
2020-06-03 22:24:08 +0300

    Update Soldier.hpp

--- Soldier.hpp
+++ Soldier.hpp
@@ -100,7 +100,7 @@ class Soldier
     void basic_action(std::vector<std::vector<Soldier*>> board,int x, int y)
     {
     Soldier* s = find_target(board,x,y);
-    s->_health = _health - _impact; //general formula for offensive soldiers
+    s->_health = s->_health - _impact; //general formula for offensive soldiers
     if(s->_health < 1)
     {
     delete s;
