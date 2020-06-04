26c1b00cbc80b05fbe7f41490f5464609e3708b7
Alex <44925899+alexchagan@users.noreply.github.com>
2020-06-04 21:25:43 +0300

    Update FootCommander.cpp

--- FootCommander.cpp
+++ FootCommander.cpp
@@ -13,10 +13,14 @@ Soldier* FootCommander::find_target(std::vector<std::vector<Soldier*>>& board,in
     return find_closest_target(board,x,y);
 }
 
+/*
+new implemtation to main_action for commander soldier to trigger all non-commander soldiers from the same type. 
+*/
 void FootCommander::main_action(std::vector<std::vector<Soldier*>>& board,int x, int y)
 {
-    basic_action(board,x,y);
+    basic_attack(board,x,y);
     
+    //go over the board and trigger all foot soldiers from same team
     for(int i = 0; i<board.size(); i++)
     {
       for(int j=0; j<board[i].size(); j++)
